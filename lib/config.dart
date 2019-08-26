2a5008a27adcc17e3ad5fb985d12531c1d6aef06
Andi Yudistira <andi.yudistira81@gmail.com>
2019-08-26 22:32:04 +0700

    Fix something

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 //const API_BASE_URL = "http://localhost:7000/v1/";
-const API_BASE_URL = "http://209.97.164.159:7000/v1/";
-const DATA_FILE_SERVER = "http://209.97.164.159:7001/data/";
+const API_BASE_URL = "http://api.ascompklaten.com/v1/";
+const DATA_FILE_SERVER = "http://fileserver.ascompklaten.com/data/";
 
 const FS_USERNAME = "admin";
 const FS_PASSWORD = "ahoyahoy";
